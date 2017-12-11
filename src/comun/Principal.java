9f25279bda34d394489323bbfe74b9cc90aebfbf
Mario1234 <30897590+Mario1234@users.noreply.github.com>
2017-12-11 15:03:20 +0100

    Principal Autowired

--- src/comun/Principal.java
+++ src/comun/Principal.java
@@ -1,24 +1,38 @@
 package comun;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
 
 import comun.config.ConfiguracionAplicacion;
 import usuario.bo.UsuarioBo;
 import usuario.modelo.Usuario;
 
+@Component
 public class Principal 
 {	
+	@Autowired UsuarioBo usuarioBo;
+	
+	private void guarda(Usuario usuario) {
+		usuarioBo.guarda(usuario);
+	}
+	private void actualiza(Usuario usuario) {
+		usuarioBo.actualiza(usuario);
+	}
+	private void elimina(Usuario usuario) {
+		usuarioBo.elimina(usuario);
+	}
     public static void main( String[] argumentos )
     {
     	AnnotationConfigApplicationContext contexto = 
                 new AnnotationConfigApplicationContext(ConfiguracionAplicacion.class);
 
-		UsuarioBo usuBo = contexto.getBean(UsuarioBo.class);
+		Principal princi = contexto.getBean(Principal.class);
 		/** insert **/
 		Usuario usuario = new Usuario();
 		usuario.setUsuarioNombre("Alfred");
 		usuario.setUsuarioApellido("Hitch");
-		usuBo.guarda(usuario);
+		princi.guarda(usuario);
 		
 		/** select **/
 		//Usuario usuario2 = usuBo.encuentraUsuarioPorId(0);
@@ -26,10 +40,10 @@ public class Principal
 		
 		/** update **/
 		usuario.setUsuarioNombre("Paco");
-		usuBo.actualiza(usuario);
+		princi.actualiza(usuario);
 		
 		/** delete **/
-		usuBo.elimina(usuario);
+		princi.elimina(usuario);
 		
 		System.out.println("Hecho");
 		contexto.close();
